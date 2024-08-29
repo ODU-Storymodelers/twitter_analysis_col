ba795e4fed18aab8a9909a862f9b492ca1f20c84
Joseph Martínez <josephms957@gmail.com>
2024-08-29 11:34:00 -0400

    added option to exclude retweets

--- notebooks/get_tweets_per_month.py
+++ notebooks/get_tweets_per_month.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
 PATH = './data/texts/colombian_valid_tweets_tone_preds_wwm.csv'
-OUTPUT_PATH = './data/texts/months/2021-02.xlsx'
-DATE = '2021-02-01'
+OUTPUT_PATH = './data/texts/months/2020-10-noRT.xlsx'
+DATE = '2020-10-01'
+include_retweets = False
 
 
 print('Extracting tweets from', DATE)
 
 df = pd.read_csv(PATH)
 df = df[df['date'] == DATE]
+
+if not include_retweets: #remove duplicated texts or Ids
+    df = df.drop_duplicates(subset=['Id'])
+    df = df.drop_duplicates(subset=['text'])
+
 df.reset_index(drop=True, inplace=True)
 
 print('Number of tweets:', df.shape[0])
