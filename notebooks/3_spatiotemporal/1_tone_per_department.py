b8384c5e570e9bfd9a9feaee4addaaf83c57b923
Joseph Martinez <josephms957@gmail.com>
2025-03-27 10:12:07 -0400

    added proportion of tone per department (tone/pos+neg+neu)

--- notebooks/3_spatiotemporal/1_tone_per_department.py
+++ notebooks/3_spatiotemporal/1_tone_per_department.py
@@ -1,103 +1,42 @@
 import pandas as pd
 
-# 1) Negative tweets per department (per 100,000 inhabitants)
+def get_tone_per_department():
+# 1) Tweets per department (per 100,000 inhabitants)
 
-df_with_depts = pd.read_csv('./data/geolocation/geo_tweets_with_depts_and_tone.csv')
+    df_with_depts = pd.read_csv('./data/geolocation/geo_tweets_with_depts_and_tone.csv')
 
-# Only keep tweets with negative tone
-df_with_depts = df_with_depts[df_with_depts['tone'] == 'negative']
+    # Group by department and tone, count tweets for each combination
+    df_counts = df_with_depts.groupby(['department', 'tone']).size().unstack(fill_value=0)
+    
+    # Rename columns to count_[tone]
+    df_counts.columns = ['count_' + tone for tone in df_counts.columns]
+    
+    # Reset index to make department a column again
+    df_with_depts = df_counts.reset_index()
 
-# Group by department and count the number of tweets
-df_with_depts = df_with_depts.groupby('department').size().reset_index(name='count')
+    df_with_depts['count_total'] = df_with_depts['count_negative'] + df_with_depts['count_neutral'] + df_with_depts['count_positive']
 
-# Sort by count in descending order
-df_with_depts = df_with_depts.sort_values(by='count', ascending=False)
+    # Load population data (DANE 2018)
+    df_population = pd.read_csv('./data/demographic/1_population_by_department_2018_DANE.csv')
 
-# Load population data (DANE 2018)
-df_population = pd.read_csv('./data/demographic/1_population_by_department_2018_DANE.csv')
+    # Merge with population data
+    df_with_depts = df_with_depts.merge(df_population, on='department', how='left')
 
-# Merge with population data
-df_with_depts = df_with_depts.merge(df_population, on='department', how='left')
+    # 2.1) get Number of negative,neutral,positive tweets per department (per 100,000 inhabitants)
+    for tone in ['negative', 'neutral', 'positive']:
+        df_with_depts['count_per_100000_' + tone] = [100000*df_with_depts['count_' + tone][i] / df_with_depts['population_2018'][i] for i in range(len(df_with_depts))]
 
-# get Number of negative tweets per department (per 100,000 inhabitants)
-df_with_depts['proportion'] = [100000*df_with_depts['count'][i] / df_with_depts['population_2018'][i] for i in range(len(df_with_depts))]
+    # 2.2) Get the proportion of each tweet tone per department (tone/(neutral+positive+negative)/population*100000)
+    for tone in ['negative', 'neutral', 'positive']:
+        df_with_depts['proportion_per_100000_' + tone] = [100000*df_with_depts['count_' + tone][i] / df_with_depts['count_total'][i] for i in range(len(df_with_depts))]
 
-# Sort by proportion in descending order
-df_with_depts = df_with_depts.sort_values(by='proportion', ascending=False)
-
-
-print("================================================")
-print("Number of negative tweets per department by population (all years)")
-print(df_with_depts)
-print("================================================")
-
-
-# Save to csv
-df_with_depts.to_csv('./notebooks/3_spatiotemporal/1_negative_tweets_per_department_standardized_100000.csv', index=False)
+    # Sort by population in descending order
+    df_with_depts = df_with_depts.sort_values(by='population_2018', ascending=False)
 
 
-
-# 2) Neutral tweets per department (per 100,000 inhabitants)
-
-df_with_depts = pd.read_csv('./data/geolocation/geo_tweets_with_depts_and_tone.csv')
-
-# Only keep tweets with neutral tone
-df_with_depts = df_with_depts[df_with_depts['tone'] == 'neutral']
-
-# Group by department and count the number of tweets
-df_with_depts = df_with_depts.groupby('department').size().reset_index(name='count')
-
-# Sort by count in descending order
-df_with_depts = df_with_depts.sort_values(by='count', ascending=False)
-
-# Merge with population data
-df_with_depts = df_with_depts.merge(df_population, on='department', how='left')
-
-# get Number of neutral tweets per department (per 100,000 inhabitants)
-df_with_depts['proportion'] = [100000*df_with_depts['count'][i] / df_with_depts['population_2018'][i] for i in range(len(df_with_depts))]
-
-# Sort by proportion in descending order
-df_with_depts = df_with_depts.sort_values(by='proportion', ascending=False)
-
-print("================================================")
-print("Number of neutral tweets per department by population (all years)")
-print(df_with_depts)
-print("================================================")
-
-
-# Save to csv
-df_with_depts.to_csv('./notebooks/3_spatiotemporal/1_neutral_tweets_per_department_standardized_100000.csv', index=False)
-
-
-
-# 3) Positive tweets per department (per 100,000 inhabitants)
-
-df_with_depts = pd.read_csv('./data/geolocation/geo_tweets_with_depts_and_tone.csv')
-
-# Only keep tweets with positive tone
-df_with_depts = df_with_depts[df_with_depts['tone'] == 'positive']  
-
-# Group by department and count the number of tweets
-df_with_depts = df_with_depts.groupby('department').size().reset_index(name='count')
-
-# Sort by count in descending order
-df_with_depts = df_with_depts.sort_values(by='count', ascending=False)
-
-
-# Merge with population data
-df_with_depts = df_with_depts.merge(df_population, on='department', how='left')
-
-# get Number of positive tweets per department (per 100,000 inhabitants)
-df_with_depts['proportion'] = [100000*df_with_depts['count'][i] / df_with_depts['population_2018'][i] for i in range(len(df_with_depts))]
-
-# Sort by proportion in descending order
-df_with_depts = df_with_depts.sort_values(by='proportion', ascending=False)
-
-print("================================================")
-print("Number of positive tweets per department by population (all years)")
-print(df_with_depts)
-print("================================================")
-
-
-# Save to csv
-df_with_depts.to_csv('./notebooks/3_spatiotemporal/1_positive_tweets_per_department_standardized_100000.csv', index=False)
+    # Save to csv
+    df_with_depts.to_csv(f'./notebooks/3_spatiotemporal/1_tweets_per_department_standardized_100000.csv', index=False)
+    
+    
+if __name__ == "__main__":
+    get_tone_per_department()
